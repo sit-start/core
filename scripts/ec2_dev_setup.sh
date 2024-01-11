33c0200950e232a67af9cb180a8ff77ff252798d
Kevin Dale <kevdale@gmail.com>
2024-01-11 14:03:26 -0700

    [aws] add pytorch, cuda, more to dev setup script
    Summary:
    
    This commit includes a setup script for setting up a new dev instance
    with the following:
    - NVIDIA Driver 535.129.03
    - CUDA 12.3.2
    - cuDNN 8.9.7.29
    - PyTorch master 2023-01-10
    - Docker 20.10.25
    
    This was installed on Amazon Linux, using ami-08a800e4b5aa90bb8, which
    already has the NVIDIA driver installed. I never successfully installed
    the drivers on a fresh Amazon Linux AMI.
    
    > [!NOTE]
    > This is archived in ami-08a800e4b5aa90bb8 and exercised in following
    > commits for the `devman` tool.
    
    Test Plan:
    Created a g5g.2xlarge instance, using ami-08a800e4b5aa90bb8, with 64GB
    root volume and run the script. Confirm expected versions are installed.
    Confirmed pytorch was properly compiled with CUDA support via
    `torch.cuda.is_available()`.

--- scripts/ec2_dev_setup.sh
+++ scripts/ec2_dev_setup.sh
@@ -1,30 +1,206 @@
 #!/usr/bin/env bash
 
-readonly USER_HOME=/home/ec2-user
-readonly MAIN_VENV_PATH="${USER_HOME}/.virtualenvs/main"
-readonly ARCH=arm64
-readonly PYTHON_VER=python3.11
+set -v
 
-function main() {
+readonly USER=ec2-user # for Amazon Linux 2023-4
+readonly USER_HOME=$(eval echo ~$USER)
+readonly PYTHON_VER=python3.11
+readonly MAIN_VENV_PATH="$USER_HOME/.virtualenvs/main"
+readonly ARCH=$(uname -m)
+readonly CUDA_HOME=/usr/local/cuda
+
+### Tools for installing packages on Amazon Linux 2023 ###
+
+function install_core_packages() {
+  echo "Installing core packages"
+  # Update all system packages to their latest versions
+  # TODO: consider upgrading
+  # yum -y upgrade --releasever=2023.3.20240108
+  yum -y update
+
   # Install development tools first
   yum -y groupinstall "Development tools"
 
-  # Install any remaining native tools
-  yum -y install emacs
+  # Install any remaining tools
+  yum -y install emacs cmake ninja-build protobuf
 
   # Install Python 3.11; -devel is necessary for some package installations, e.g., psutil
-  yum -y install "${PYTHON_VER}" "${PYTHON_VER}-devel" "${PYTHON_VER}-pip" python3-devel
-
-  # Create and activate a default Python virtual environment
-  "${PYTHON_VER}" -m venv "${MAIN_VENV_PATH}"
-  source "${MAIN_VENV_PATH}"/bin/activate
-
-  # Install Python packages
-  pip3 install --upgrade pip
-  pip3 install ipykernel torch torchvision ipympl matplotlib jupyterthemes mplcursors h5py scipy tensorboard grpcio-tools torch-tb-profiler imageio imageio-ffmpeg torch-tb-profiler hydra-core jupyter jupyterlab_widgets Pillow pandas numpy urllib3 ffmpeg scikit-learn tqdm boto3 regex
+  yum -y install python3-devel "$PYTHON_VER" "$PYTHON_VER-devel" "$PYTHON_VER-pip"
 
   # Install yadm
   curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/local/bin/yadm
+}
+
+function install_gflags_from_source() {
+  echo "Installing gflags"
+  version="2.2.2"
+  mkdir gflags
+  pushd gflags
+  wget -nv https://github.com/gflags/gflags/archive/refs/tags/v$version.tar.gz
+  tar -xzf v$version.tar.gz
+  cd gflags-$version
+  mkdir build && cd build
+  cmake ..
+  make install
+  popd
+}
+
+function install_glog_from_source() {
+  echo "Installing glog"
+  version="0.6.0"
+  mkdir glog
+  pushd glog
+  wget -nv https://github.com/google/glog/archive/refs/tags/v$version.tar.gz
+  tar -xzf v$version.tar.gz
+  cd glog-$version
+  mkdir build && cd build
+  cmake ..
+  make install
+  popd
+}
+
+function install_ffmpeg() {
+  # https://www.johnvansickle.com/ffmpeg/faq/
+  echo "Installing ffmpeg"
+  if [ "$ARCH" == "aarch64" ]; then
+    arch="arm64"
+  else # x86_64
+    arch="amd64"
+  fi
+
+  mkdir ffmpeg
+  pushd ffmpeg
+
+  build="ffmpeg-6.0.1-$arch-static"
+  wget -nv "https://www.johnvansickle.com/ffmpeg/old-releases/$build.tar.xz"
+  tar -xvf "$build.tar.xz"
+  mv "$build" /usr/local/bin/ffmpeg
+  ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
+  ln -s /usr/local/bin/ffmpeg/ffprobe /usr/bin/ffprobe
+
+  popd
+}
+
+function install_python() {
+  echo "Installing Python and Python packages"
+  # Create and activate a default Python virtual environment
+  "$PYTHON_VER" -m venv "$MAIN_VENV_PATH"
+  source "$MAIN_VENV_PATH/bin/activate"
+
+  # Install Python packages
+  pip3 install --upgrade pip
+  pip3 install ipykernel ipympl matplotlib jupyterthemes mplcursors h5py scipy tensorboard grpcio-tools torch-tb-profiler imageio imageio-ffmpeg torch-tb-profiler hydra-core jupyter jupyterlab_widgets Pillow pandas numpy urllib3 ffmpeg scikit-learn tqdm boto3 regex pytest determined typing-extensions sympy filelock fsspec networkx pyyaml
+}
+
+function install_docker() {
+  echo "Installing Docker and the NVIDIA container toolkit"
+  # Install/setup docker; requires a reboot for group settings to take effect and for docker to start
+  yum install docker
+  usermod -aG docker $USER
+  systemctl enable docker.service
+  systemctl enable containerd.service
+
+  # NVIDIA container toolkit. Used by determined-ai
+  #
+  # NOTE: nvidia-docker is deprecated; using nvidia-container-toolkit
+  # instead, which may require some tweaking to play nicely with
+  # determined-ai
+  curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo |
+    tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+  yum install -y nvidia-container-toolkit
+}
+
+function install_nvidia() {
+  echo "Installing NVIDIA driver, CUDA, and cuDNN"
+  # based on this: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/install-nvidia-driver.html
+  # NOTE: this fails on amazon linux 2023, but it works on ami-08a800e4b5aa90bb8,
+  # which is based on amazon linux 2023. so we can continue to use the new AMI
+  # and just update the drivers here on top of what's already installed in the AMI.
+  # TODO: appending to blacklist.conf was required on Ubuntu; not sure
+  # it's necessary here
+  cat <<EOF | sudo tee --append /etc/modprobe.d/blacklist.conf
+blacklist vga16fb
+blacklist nouveau
+blacklist rivafb
+blacklist nvidiafb
+blacklist rivatv
+EOF
+  wget -nv "https://us.download.nvidia.com/tesla/535.129.03/NVIDIA-Linux-${ARCH}-535.129.03.run"
+  sh "NVIDIA-Linux-${ARCH}-535.129.03.run" --silent --disable-nouveau --tmpdir="$USER_HOME/tmp"
+
+  # CUDA
+  suffix=
+  if [ "$ARCH" == "aarch64" ]; then
+    suffix="_sbsa"
+  fi
+  cuda_installer="cuda_12.3.2_545.23.08_linux$suffix.run"
+  wget -nv "https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/$cuda_installer"
+  sh "$cuda_installer" --silent --override \
+    --toolkit --samples --toolkitpath=/usr/local/cuda-12.3 \
+    --samplespath="$CUDA_HOME" --no-opengl-libs --tmpdir="$USER_HOME/tmp"
+
+  # cuDNN
+  if [ "$ARCH" == "aarch64" ]; then
+    arch="sbsa"
+  else
+    arch="x86_64"
+  fi
+  archive="cudnn-linux-$arch-8.9.7.29_cuda12-archive"
+  wget -nv "https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-$arch/$archive.tar.xz"
+  tar -xf "$archive.tar.xz"
+  cp -P "$archive/include/*" "$CUDA_HOME/include/"
+  cp -P "$archive/lib/*" "$CUDA_HOME/lib64/"
+  chmod a+r "$CUDA_HOME/lib64/*"
+}
+
+function install_pytorch_from_source() {
+  echo "Installing Pytorch from source"
+  # Download and install ccache for faster compilation
+  wget -nv https://github.com/ccache/ccache/releases/download/v4.8.3/ccache-4.8.3.tar.xz
+  tar -xf ccache-4.8.3.tar.xz
+  pushd ccache-4.8.3
+  cmake .
+  make -j $CPUS
+  make install
+  popd
+
+  # Clone PyTorch repository and install from source
+  # NOTE: this may OOM with < 16GB RAM; this can be mitigated by setting
+  #       CMAKE_BUILD_PARALLEL_LEVEL to a lower value
+  # NOTE: it's probably better to use the latest release, e.g.,
+  #        wget -nv https://github.com/pytorch/pytorch/archive/refs/tags/v2.1.2.tar.gz
+  #        tar -xzf v2.1.2.tar.gz
+  #        pushd pytorch-2.1.2
+  git clone --recursive https://github.com/pytorch/pytorch.git
+  source "$MAIN_VENV_PATH/bin/activate"
+  LD_LIBRARY_PATH="$CUDA_HOME/lib64/:$LD_LIBRARY_PATH"
+  PATH="$CUDA_HOME/bin:$PATH"
+  CMAKE_CUDA_COMPILER=$(which nvcc)
+  
+  pip3 install -r requirements.txt
+  python3 setup.py install
+  
+  popd
+
+  # Refresh the dynamic linker run-time bindings
+  ldconfig
+}
+
+function main() {
+  # use the larger root volume for temp files during script exection
+  mkdir -p "$USER_HOME/tmp"
+  pushd "$USER_HOME/tmp"
+
+  install_core_packages
+  install_gflags_from_source
+  install_glog_from_source
+  install_ffmpeg
+  install_python
+  install_nvidia
+  install_docker
+  install_pytorch_from_source
+
+  popd
 }
 
 main "${@}"
