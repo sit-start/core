d1c66d278fff72ec4ef667de0e1a38172c5952e6
Kevin Dale <kevdale@gmail.com>
2024-01-24 11:07:10 -0700

    [data] simplify cloudpathlib import
    Summary:
    Allows for `from ktd.cloudpathlib import CloudPath` to properly register
    http(s) paths/clients.
    
    Test Plan:
    ```
    from ktd.cloudpathlib import CloudPath
    path = HttpsPath("https://en.wikipedia.org/wiki/Trie")
    ```

---
+++ python/ktd/cloudpathlib/__init__.py
@@ -0,0 +1,12 @@
+"""Wrapper around cloudpathlib so that http(s) clients/paths are registered"""
+from cloudpathlib import CloudPath
+
+from .http_client import HttpClient
+from .http_path import HttpPath, HttpsPath
+
+__all__ = [
+    "CloudPath",
+    "HttpClient",
+    "HttpPath",
+    "HttpsPath",
+]
