7a7cd6aebcdfe0c1e1ba0a6a13ffc4f67b034d78
Kevin Dale <kevdale@gmail.com>
2024-01-25 06:30:43 -0700

    [aws] remove `ssh keyscan` for github keys

--- python/ktd/aws/devman.py
+++ python/ktd/aws/devman.py
@@ -6,6 +6,8 @@ import sys
 from pathlib import Path
 from typing import Type
 
+import json5
+from ktd.cloudpathlib import CloudPath
 import boto3
 import ktd.logging
 from boto3.resources.base import ServiceResource
@@ -45,11 +47,36 @@ _CMD_PREFIX = "_cmd_"
 # TODO: add a tag to the stack and/or instance to make filtering easy
 
 
+def _github_ssh_keys(use_cached: bool = True) -> list[str]:
+    if use_cached:
+        return [
+            "ssh-ed25519 "
+            "AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl",
+            "ecdsa-sha2-nistp256 "
+            "AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7op"
+            "KgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=",
+            "ssh-rsa "
+            "AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1"
+            "aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd"
+            "1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKg"
+            "bLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwD"
+            "Csw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPe"
+            "AUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5Q"
+            "UCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2"
+            "zB3nAZp+S5hpQs+p1vN1/wsjk=",
+        ]
+    path = CloudPath("https://api.github.com/meta")
+    meta = json5.loads(path.read_text())
+    return meta.get("ssh_keys", []) if isinstance(meta, dict) else []
+
+
 def _clone_dotfiles_and_repos(instance_name: str) -> None:
-    logger.info(f"[{instance_name}] Making initial SSH connection")
-    # yadm and git commands assume identity forwarding is setup in the SSH config
-    # FIXME: the ssh-keyscan method is frowned upon.
-    _ = subprocess.call(
+    # note - yadm and git commands assume identity forwarding is setup in the SSH config
+
+    logger.info(f"[{instance_name}] Updating known hosts over initial SSH connection")
+    # add github to known hosts, with retries on the initial connection
+    github_known_hosts = "\n".join(f"github.com {v}" for v in _github_ssh_keys())
+    subprocess.call(
         [
             "ssh",
             "-o",
@@ -57,9 +84,8 @@ def _clone_dotfiles_and_repos(instance_name: str) -> None:
             "-o",
             "StrictHostKeyChecking no",
             instance_name,
-            "ssh-keyscan -H github.com >> ~/.ssh/known_hosts",
-        ],
-        stderr=subprocess.DEVNULL,
+            f"echo '{github_known_hosts}' >> ~/.ssh/known_hosts",
+        ]
     )
 
     logger.info(f"[{instance_name}] Cloning dotfiles and repos")
