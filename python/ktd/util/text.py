10c0aff530050850c35b30475e41a5185337d351
Kevin Dale <kevdale@gmail.com>
2024-02-19 13:41:54 -0700

    [util] add `terminal_hyperlink` for urls

--- python/ktd/util/text.py
+++ python/ktd/util/text.py
@@ -8,3 +8,10 @@ def strip_ansi_codes(s: str) -> str:
 def truncate(s: str, max_len: int = 50) -> str:
     suffix = "..."
     return s if len(s) <= max_len else s[: max_len - len(suffix)] + suffix
+
+
+# https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
+def terminal_hyperlink(url: str, text: str) -> str:
+    # Works as expected on iTerm2, but "file://" URLs don't work in VS Code's
+    # terminal
+    return f"\033]8;;{url}\033\\{text}\033]8;;\033\\"
