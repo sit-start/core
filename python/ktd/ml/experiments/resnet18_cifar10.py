e92c54385ded5f2c4bc32e0db5d2387266091e4e
Kevin Dale <kevdale@gmail.com>
2024-03-14 09:05:18 -0600

    [ray][dm] simplify job submission

--- python/ktd/ml/experiments/resnet18_cifar10.py
+++ python/ktd/ml/experiments/resnet18_cifar10.py
@@ -114,7 +114,6 @@ def ResNet18(num_classes: int, dropout_p: float, with_batchnorm: bool = True):
 def main():
     config = {
         "debug": False,
-        "strict_provenance": False,
         "tune": {
             "num_samples": 1,
             "long_trial_names": True,
