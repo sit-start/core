7398c19e3c9b21ee790e9f596de0ff2c51de9e14
kevdale <kevdale@gmail.com>
2024-03-19 08:58:33 -0600

    [ml] cleanup exemplar data module
    Summary:
    
    This commit
    - simplifies data downloading by using `prepare_data`, with
      `prepare_data_per_node = True` so that each node downloads the dataset`
    - removes the unnecessary shuffle seeding for the train dataloader
    - extracts the smoketest data module into a separate file
    - extracts dataset splitting into util.py for reuse

--- python/ktd/ml/util.py
+++ python/ktd/ml/util.py
@@ -1,5 +1,21 @@
 import torch
+from torch.utils.data import Dataset, random_split
 
 
 def hash_tensor(x: torch.Tensor) -> int:
     return hash(tuple(x.cpu().reshape(-1).tolist()))
+
+
+def split_dataset(
+    dataset: Dataset, train_split: float, train: bool = True, seed: int | None = 42
+) -> Dataset:
+    assert hasattr(dataset, "__len__"), "Dataset must implement __len__"
+
+    len_dataset = len(dataset)  # type: ignore[arg-type]
+    n_train = int(len_dataset * train_split)
+    lengths = [n_train, len_dataset - n_train]
+    generator = torch.Generator().manual_seed(seed) if seed else None
+
+    train_dataset, val_dataset = random_split(dataset, lengths, generator=generator)
+
+    return train_dataset if train else val_dataset
