ffddd53a94638c80871c24f2ce68f67d45d45cd9
kevdale <kevdale@gmail.com>
2024-03-19 08:58:33 -0600

    [ml] cleanup exemplar data module
    Summary:
    
    This commit
    - simplifies data downloading by using `prepare_data`, with
      `prepare_data_per_node = True` so that each node downloads the dataset`
    - removes the unnecessary shuffle seeding for the train dataloader
    - extracts the smoketest data module into a separate file
    - extracts dataset splitting into util.py for reuse

--- python/ktd/ml/data/cifar10.py
+++ python/ktd/ml/data/cifar10.py
@@ -1,12 +1,10 @@
 import os
-from pathlib import Path
 
 import pytorch_lightning as pl
-import torch
-import torchvision
-from filelock import FileLock
+import torchvision.datasets as datasets
 from ktd.logging import get_logger
-from torch.utils.data import DataLoader, Subset, random_split
+from ktd.ml.util import split_dataset
+from torch.utils.data import DataLoader
 from torchvision.transforms import (
     Compose,
     Normalize,
@@ -31,126 +29,58 @@ class CIFAR10(pl.LightningDataModule):
     ):
         super().__init__()
         self.batch_size = batch_size
-        self.data_dir = Path(data_dir) if data_dir else Path.cwd()
+        self.data_dir = str(data_dir) if data_dir else os.getcwd()
         self.train_split = train_split
         self.n_workers = n_workers
-        self.prepare_data_per_node = False
+        self.prepare_data_per_node = True
         self.augment = augment
-        self.generator = torch.Generator().manual_seed(42)
+
+    def prepare_data(self) -> None:
+        datasets.CIFAR10(self.data_dir, train=True, download=True)
+        datasets.CIFAR10(self.data_dir, train=False, download=True)
 
     def setup(self, stage: str | None = None):
         normalization = Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         augmentation = [RandomCrop(32, padding=4), RandomHorizontalFlip()]
+        seed = 42
 
-        test_transform = Compose([ToTensor(), normalization])
+        val_and_test_transform = Compose([ToTensor(), normalization])
         train_transform = Compose(
             (augmentation if self.augment else []) + [ToTensor(), normalization]
         )
 
-        with FileLock(self.data_dir / "data.lock"):
-            train = torchvision.datasets.CIFAR10(
-                root=str(self.data_dir),
-                train=True,
-                download=True,
-                transform=train_transform,
-            )
-
-        # Load the training dataset twice so that we can specify different
-        # transforms for train and val
-        val = torchvision.datasets.CIFAR10(
-            root=str(self.data_dir),
-            train=True,
-            download=False,
-            transform=test_transform,
+        train_dataset = datasets.CIFAR10(
+            self.data_dir, train=True, transform=train_transform
+        )
+        self.train_dataset = split_dataset(
+            train_dataset, self.train_split, train=True, seed=seed
         )
 
-        n_train = int(len(train) * self.train_split)
-        self.train, not_train = random_split(
-            train, [n_train, len(train) - n_train], generator=self.generator
+        val_dataset = datasets.CIFAR10(
+            self.data_dir, train=True, transform=val_and_test_transform
         )
-        self.val = Subset(val, not_train.indices)
+        self.val_dataset = split_dataset(
+            val_dataset, self.train_split, train=False, seed=seed
+        )
 
-        self.test = torchvision.datasets.CIFAR10(
-            root=str(self.data_dir),
-            train=False,
-            download=False,
-            transform=test_transform,
+        self.test_dataset = datasets.CIFAR10(
+            self.data_dir, train=False, transform=val_and_test_transform
         )
 
     def train_dataloader(self):
         return DataLoader(
-            self.train,
+            self.train_dataset,
             batch_size=self.batch_size,
             num_workers=self.n_workers,
             shuffle=True,
-            generator=self.generator,
         )
 
     def val_dataloader(self):
         return DataLoader(
-            self.val, batch_size=self.batch_size, num_workers=self.n_workers
+            self.val_dataset, batch_size=self.batch_size, num_workers=self.n_workers
         )
 
     def test_dataloader(self):
         return DataLoader(
-            self.test, batch_size=self.batch_size, num_workers=self.n_workers
+            self.test_dataset, batch_size=self.batch_size, num_workers=self.n_workers
         )
-
-
-class SmokeTestCIFAR10(pl.LightningDataModule):
-    NUM_CLASSES: int = 10
-
-    def __init__(
-        self,
-        batch_size: int = 128,
-        num_train: int = 1280,
-        train_split: float = 0.8,
-        num_test: int = 128,
-        num_classes: int = 10,
-        img_shape: tuple[int, int, int] = (3, 32, 32),
-    ):
-        super().__init__()
-        self.batch_size = batch_size
-        self.num_train = num_train
-        self.train_split = train_split
-        self.num_test = num_test
-        self.prepare_data_per_node = False
-        self.num_classes = num_classes
-        self.img_shape = img_shape
-        self.generator = torch.Generator().manual_seed(hash("SmokeTestCIFAR10"))
-
-    def setup(self, stage: str | None = None):
-        num_train = int(self.num_train * self.train_split)
-        num_val = self.num_train - num_train
-        self.train = torchvision.datasets.FakeData(
-            num_train,
-            self.img_shape,
-            num_classes=self.num_classes,
-            transform=ToTensor(),
-        )
-        self.val = torchvision.datasets.FakeData(
-            num_val,
-            self.img_shape,
-            num_classes=self.num_classes,
-            transform=ToTensor(),
-        )
-        self.test = torchvision.datasets.FakeData(
-            self.num_test,
-            self.img_shape,
-            num_classes=self.num_classes,
-            transform=ToTensor(),
-        )
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.train,
-            batch_size=self.batch_size,
-            shuffle=True,
-            generator=self.generator,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(self.val, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.test, batch_size=self.batch_size)
