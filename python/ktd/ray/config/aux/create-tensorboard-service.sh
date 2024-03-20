b62befe8e0e32cb2d239a8fc4be5c951c081704a
kevdale <kevdale@gmail.com>
2024-03-19 18:35:08 -0600

    [ray] add tensorboard service
    Fixes #19

---
+++ python/ktd/ray/config/aux/create-tensorboard-service.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# https://github.com/ray-project/ray/blob/master/doc/azure/azure-init.sh
+
+if [ $# -gt 1 ]; then
+  echo "Usage: $0 [service_user]"
+  exit 1
+elif [ $# -eq 1 ]; then
+  user=$1
+else
+  user=$USER
+fi
+
+log_dir="/home/$user/ray_results"
+script_path="/home/$user/tensorboard.sh"
+
+cat >"$script_path" <<EOM
+#!/usr/bin/env bash
+mkdir -p $log_dir
+tensorboard --port 6006 --logdir=$log_dir
+EOM
+
+chmod +x "$script_path"
+
+cat >/etc/systemd/system/tensorboard.service <<EOM
+[Unit]
+   Description=TensorBoard
+
+[Service]
+   Type=simple
+   User=$user
+   ExecStart=/bin/bash -l $script_path
+
+[Install]
+WantedBy=multi-user.target
+EOM
