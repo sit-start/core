1c0cea10abff716b19e3bd05a11beeeedf9dceb4
Kevin Dale <kevdale@gmail.com>
2024-05-02 09:13:52 -0600

    [sit][ray] clearer config/script input, fix `down`

--- python/ktd/app/sit/sub/ray.py
+++ python/ktd/app/sit/sub/ray.py
@@ -1,4 +1,3 @@
-import subprocess
 import sys
 from os.path import expanduser, realpath
 from pathlib import Path
@@ -25,22 +24,25 @@ from ktd.util.vscode import open_vscode_over_ssh
 
 DEFAULT_CONFIG = "main"
 CONFIG_ROOT = f"{PYTHON_ROOT}/ktd/ray/config/cluster"
+SCRIPT_ROOT = f"{PYTHON_ROOT}/ktd/ml/experiments"
 FORWARDED_PORTS = {
     "Ray Dashboard": 8265,
     "Prometheus": 9090,
     "Grafana": 3000,
     "TensorBoard": 6006,
 }
-SCRIPT_PATH_DEFAULT = f"{PYTHON_ROOT}/ktd/ml/experiments/{{script_name}}.py"
 
 
 app = typer.Typer()
 logger = get_logger(__name__, format="simple")
 
+
 # Arguments and options
-ConfigArg = Annotated[
+ConfigOpt = Annotated[
     str,
-    Argument(help=f"The Ray cluster config in '{CONFIG_ROOT}'."),
+    Option(
+        help=f"The Ray cluster config path, or filename or stem in {CONFIG_ROOT!r}."
+    ),
 ]
 ProfileOpt = Annotated[
     Optional[str],
@@ -126,16 +128,10 @@ KillOpt = Annotated[
     bool,
     Option("--kill", help="Terminate all instances.", show_default=False),
 ]
-ScriptPathArg = Annotated[
+ScriptArg = Annotated[
     str,
     Argument(
-        help="The path to the script to run, required if --script-name is not specified."
-    ),
-]
-ScriptNameOpt = Annotated[
-    Optional[str],
-    Option(
-        help="The name of the script to run, required if script_path is not specified.",
+        help=f"The script path, or filename or stem in {SCRIPT_ROOT!r}.",
         show_default=False,
     ),
 ]
@@ -149,19 +145,44 @@ RestartOpt = Annotated[
 ]
 
 
+def _resolve_input_path(input: str, root: str, extensions: list[str]) -> str:
+    for path in [
+        f"{root}/{input}",
+        *[f"{root}/{input}.{ext}" for ext in extensions],
+        input,
+    ]:
+        if Path(path).expanduser().exists():
+            return path
+    raise FileNotFoundError(
+        f"File/stem {input!r} not found in {root!r} with extensions "
+        f"{' '.join(f'{e!r}' for e in extensions)}."
+    )
+
+
+def _resolve_config_path(config: str) -> str:
+    return _resolve_input_path(config, CONFIG_ROOT, ["yaml", "yml"])
+
+
 def _ray_up(
-    config: str = "main",
+    config_path: str,
+    cluster_name: str,
     min_workers: int | None = None,
     max_workers: int | None = None,
     no_restart: bool = False,
     restart_only: bool = False,
-    cluster_name: str | None = None,
     prompt: bool = False,
     verbose: bool = False,
     show_output: bool = False,
     no_config_cache: bool = False,
 ) -> None:
-    cmd = ["ray", "up", f"{CONFIG_ROOT}/{config}.yaml"]
+    cmd = [
+        "ray",
+        "up",
+        config_path,
+        "--disable-usage-stats",
+        "--cluster-name",
+        cluster_name,
+    ]
     if min_workers is not None:
         cmd += ["--min-workers", str(min_workers)]
     if max_workers is not None:
@@ -170,10 +191,6 @@ def _ray_up(
         cmd.append("--no-restart")
     if restart_only:
         cmd.append("--restart-only")
-    if cluster_name:
-        cmd += ["--cluster-name", cluster_name]
-    else:
-        cluster_name = config
     if not prompt:
         cmd.append("--yes")
     if verbose:
@@ -207,21 +224,19 @@ def stop_jobs() -> None:
 # TODO: support additional user-specified repos on local+remote hosts
 @app.command()
 def submit(
-    script_path: ScriptPathArg = SCRIPT_PATH_DEFAULT,
-    script_name: ScriptNameOpt = None,
-    config: ConfigArg = DEFAULT_CONFIG,
+    script: ScriptArg,
+    config: ConfigOpt = DEFAULT_CONFIG,
     cluster_name: ClusterNameOpt = None,
     restart: RestartOpt = False,
 ) -> None:
     """Run a job on a Ray cluster."""
-    # get script name
-    if not script_name and script_path == SCRIPT_PATH_DEFAULT:
-        raise RuntimeError("exp_name or exp_path must be provided")
+    script_path = _resolve_input_path(script, SCRIPT_ROOT, ["py", "sh"])
+    config_path = _resolve_config_path(config)
 
     def _resolve(path: str) -> str:
         return realpath(expanduser(path))
 
-    script_path = _resolve(script_path.format(script_name=script_name))
+    script_path = Path(_resolve(script_path or f"{SCRIPT_ROOT}/{script}"))
 
     # get the script path's containing repo
     try:
@@ -232,23 +247,23 @@ def submit(
 
     # ensure the repo is in the cluster config's `file_mounts`, which maps
     # cluster paths to local paths for syncing local -> head -> worker
-    config_path = Path(CONFIG_ROOT) / f"{config}.yaml"
-    config_data = yaml.load(config_path.read_text(), Loader=yaml.FullLoader)
-    repo_path = Path(repo.working_dir).resolve()
+    config_data = yaml.load(Path(config_path).read_text(), Loader=yaml.SafeLoader)
+    repo_path = Path(repo.working_dir)
     mounts = {dst: _resolve(src) for dst, src in config_data["file_mounts"].items()}
     mount = next((m for m in mounts.items() if repo_path.is_relative_to(m[1])), None)
     if not mount:
         msg = f"Repo {repo.working_dir!r} not in file_mounts and cannot be synced."
-        raise RuntimeError(msg)
-    cluster_script_path = f"{mount[0]}/{str(Path(script_path).relative_to(mount[1]))}"
+        logger.error(msg)
+        sys.exit(-1)
+    cluster_script_path = f"{mount[0]}/{str(script_path.relative_to(mount[1]))}"
 
     # invoke ray-up, syncing file mounts and running setup commands
     # even if the config hasn't changed
     logger.info("Running 'ray up' to sync files and run setup commands")
     _ray_up(
-        config=config,
+        config_path=config_path,
+        cluster_name=cluster_name or Path(config_path).stem,
         no_restart=not restart,
-        cluster_name=cluster_name,
         no_config_cache=True,
     )
 
@@ -268,7 +283,7 @@ def submit(
 
 @app.command()
 def up(
-    config: ConfigArg = DEFAULT_CONFIG,
+    config: ConfigOpt = DEFAULT_CONFIG,
     profile: ProfileOpt = None,
     min_workers: MinWorkersOpt = None,
     max_workers: MaxWorkersOpt = None,
@@ -282,14 +297,17 @@ def up(
     no_config_cache: NoConfigCacheOpt = False,
 ) -> None:
     """Create or update a Ray cluster."""
+    config_path = _resolve_config_path(config)
+    cluster_name = cluster_name or Path(config_path).stem
+
     # invoke ray up
     _ray_up(
-        config=config,
+        config_path=config_path,
+        cluster_name=cluster_name,
         min_workers=min_workers,
         max_workers=max_workers,
         no_restart=no_restart,
         restart_only=restart_only,
-        cluster_name=cluster_name,
         prompt=prompt,
         verbose=verbose,
         show_output=show_output,
@@ -299,7 +317,6 @@ def up(
     # 5s is usually sufficient for the minimal workers to be in the
     # running state after the Ray cluster is up
     sleep(5)
-    cluster_name = cluster_name or config
     session = get_aws_session(profile)
     update_ssh_config_for_instances_with_name(
         session, instance_name=f"ray-{cluster_name}-*"
@@ -321,8 +338,9 @@ def up(
 
 @app.command()
 def down(
-    cluster_name: ClusterNameOpt = None,
+    config: ConfigOpt = DEFAULT_CONFIG,
     profile: ProfileOpt = None,
+    cluster_name: ClusterNameOpt = None,
     workers_only: WorkersOnlyOpt = False,
     keep_min_workers: KeepMinWorkersOpt = False,
     prompt: PromptOpt = False,
@@ -331,20 +349,9 @@ def down(
     show_output: ShowOutputOpt = False,
 ) -> None:
     """Tear down a Ray cluster."""
-    cluster_name = cluster_name or DEFAULT_CONFIG
-
-    cluster_head_name = f"ray-{cluster_name}-head"
-    cluster_names = f"ray-{cluster_name}-*"
-    worker_names = f"ray-{cluster_name}-workers"
-
-    logger.info(f"Closing SSH connections to {cluster_head_name}")
-    try:
-        close_ssh_connection(cluster_head_name, check=True)
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Failed to close SSH connections: {e}")
-
-    logger.info(f"[{cluster_name}] Tearing down Ray cluster")
-    cmd = ["ray", "down", f"{CONFIG_ROOT}/{cluster_name}.yaml"]
+    config_path = _resolve_config_path(config)
+    cluster_name = cluster_name or Path(config_path).stem
+    cmd = ["ray", "down", config_path, "--cluster-name", cluster_name]
     if workers_only:
         cmd.append("--workers-only")
     if keep_min_workers:
@@ -354,35 +361,36 @@ def down(
     if verbose:
         cmd.append("--verbose")
 
+    logger.info(f"[{cluster_name}] Tearing down Ray cluster")
+
     if show_output:
         run(cmd, output="std")
     else:
         run(cmd + ["--log-color", "false"], output="file")
 
     session = get_aws_session(profile)
+    instance_names = (
+        f"ray-{cluster_name}-worker" if workers_only else f"ray-{cluster_name}-*"
+    )
 
     if kill and keep_min_workers:
         logger.warning("Killing instances with keep_min_workers is not implemented")
     elif kill:
-        instance_name = worker_names if workers_only else cluster_names
-        logger.info(f"[{instance_name}] Killing instances")
-        kill_instances_with_name(session, instance_name, update_ssh_config=False)
+        logger.info(f"[{instance_names}] Killing instances")
+        kill_instances_with_name(session, instance_names, update_ssh_config=False)
 
-    update_ssh_config_for_instances_with_name(session, instance_name=cluster_names)
+    update_ssh_config_for_instances_with_name(session, instance_name=instance_names)
 
 
 @app.command()
-def monitor(
-    config: str = Argument(
-        help=f"The Ray cluster config in '{CONFIG_ROOT}'.", default=DEFAULT_CONFIG
-    ),
-) -> None:
+def monitor(config: ConfigOpt = DEFAULT_CONFIG) -> None:
     """Monitor autoscaling on a Ray cluster."""
+    config_path = _resolve_config_path(config)
     log_path = "/tmp/ray/session_latest/logs/monitor*"
     cmd = [
         "ray",
         "exec",
-        f"{CONFIG_ROOT}/{config}.yaml",
+        config_path,
         "--",
         f"tail -n 100 -f {log_path}",
     ]
