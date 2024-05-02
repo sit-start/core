886961890137e89eeba838e41166d2464f0c6311
Kevin Dale <kevdale@gmail.com>
2024-05-02 07:51:12 -0600

    [test] smoketest experiment

--- test/ml/test_ml_ray.py
+++ test/ml/test_ml_ray.py
@@ -1,22 +1,30 @@
-from typing import Any
+import tempfile
 
 import pytest
 from ray.cluster_utils import Cluster
 
-from ktd.ml.ray import train_with_ray
-from ktd.ml.train import DataModuleCreator, TrainingModuleCreator
+from ktd.ml.experiments.image_multiclass_smoketest import (
+    data_module_creator,
+    train_with_ray_config,
+    tune_with_ray_config,
+    training_module_creator,
+)
+from ktd.ml.ray import train_with_ray, tune_with_ray
 
 
 @pytest.mark.slow
-def test_train_with_ray(
-    ray_cluster: Cluster,
-    config: dict[str, Any],
-    smoketest_training_module_creator: TrainingModuleCreator,
-    smoketest_data_module_creator: DataModuleCreator,
-):
-    train_with_ray(
-        config, smoketest_training_module_creator, smoketest_data_module_creator
-    )
+def test_train_with_ray(ray_cluster: Cluster):
+    with tempfile.TemporaryDirectory() as storage_path:
+        config = train_with_ray_config(storage_path)
+        train_with_ray(config, training_module_creator, data_module_creator)
+    # TODO: add checks for checkpoints, logging, # workers used,
+    # tensorboard data, repo state
 
+
+@pytest.mark.slow
+def test_tune_with_ray(ray_cluster: Cluster):
+    with tempfile.TemporaryDirectory() as storage_path:
+        config = tune_with_ray_config(storage_path)
+        tune_with_ray(config, training_module_creator, data_module_creator)
     # TODO: add checks for checkpoints, logging, # workers used,
     # tensorboard data, repo state
--- test/ml/test_ml_train.py
+++ test/ml/test_ml_train.py
@@ -1,53 +1,47 @@
-import os
 import glob
-from typing import Any
+import os
+import tempfile
 
 import pytest
 
 import ktd.ml.train
-from ktd.ml.train import DataModuleCreator, TrainingModuleCreator
+from ktd.ml.experiments.image_multiclass_smoketest import (
+    data_module_creator,
+    train_config,
+    training_module_creator,
+)
 
 
 @pytest.mark.slow
-def test_train(
-    train_config: dict[str, Any],
-    smoketest_training_module_creator: TrainingModuleCreator,
-    smoketest_data_module_creator: DataModuleCreator,
-    caplog,
-):
-    ktd.ml.train.train(
-        train_config, smoketest_training_module_creator, smoketest_data_module_creator
-    )
+def test_train(caplog):
+    with tempfile.TemporaryDirectory() as storage_path:
+        config = train_config(storage_path)
+        ktd.ml.train.train(config, training_module_creator, data_module_creator)
 
-    data_module = smoketest_data_module_creator(train_config)
-    data_module.setup(stage="fit")
-    train = getattr(data_module, "train", None)
-    assert train
-    num_batches = (len(train) - 1) // train_config["batch_size"] + 1
-    log_root = f"{train_config['storage_path']}/lightning_logs/version_0"
-    logs = [log for log in caplog.records if "train_loss" in log.message]
+        data_module = data_module_creator(config)
+        data_module.setup(stage="fit")
+        train = getattr(data_module, "train", None)
+        assert train
+        num_batches = (len(train) - 1) // config["batch_size"] + 1
+        log_root = f"{config['storage_path']}/lightning_logs/version_0"
+        logs = [log for log in caplog.records if "train_loss" in log.message]
 
-    assert len(logs) == num_batches * train_config["max_num_epochs"]
-    assert any(all(k in msg for k in train_config.keys()) for msg in caplog.messages)
-    assert os.path.exists(f"{log_root}/hparams.yaml")
-    assert glob.glob(f"{log_root}/events.out.tfevents.*")
-    assert glob.glob(f"{log_root}/checkpoints/*.ckpt")
+        assert len(logs) == num_batches * config["max_num_epochs"]
+        assert any(all(k in msg for k in config.keys()) for msg in caplog.messages)
+        assert os.path.exists(f"{log_root}/hparams.yaml")
+        assert glob.glob(f"{log_root}/events.out.tfevents.*")
+        assert glob.glob(f"{log_root}/checkpoints/*.ckpt")
 
 
 @pytest.mark.slow
-def test_test(
-    train_config: dict[str, Any],
-    smoketest_training_module_creator: TrainingModuleCreator,
-    smoketest_data_module_creator: DataModuleCreator,
-    caplog,
-) -> None:
-    ktd.ml.train.test(
-        train_config, smoketest_training_module_creator, smoketest_data_module_creator
-    )
+def test_test(caplog) -> None:
+    with tempfile.TemporaryDirectory() as storage_path:
+        config = train_config(storage_path)
+        ktd.ml.train.test(config, training_module_creator, data_module_creator)
 
-    log_root = f"{train_config['storage_path']}/lightning_logs/version_0"
-    logs = [log for log in caplog.records if "test_loss" in log.message]
+        log_root = f"{config['storage_path']}/lightning_logs/version_0"
+        logs = [log for log in caplog.records if "test_loss" in log.message]
 
-    assert len(logs) == 1
-    assert os.path.exists(f"{log_root}/hparams.yaml")
-    assert glob.glob(f"{log_root}/events.out.tfevents.*")
+        assert len(logs) == 1
+        assert os.path.exists(f"{log_root}/hparams.yaml")
+        assert glob.glob(f"{log_root}/events.out.tfevents.*")
