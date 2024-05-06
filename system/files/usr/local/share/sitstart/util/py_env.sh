acac4634ed960d22ee2b26f54785b9e6716c33e0
Kevin Dale <kevdale@gmail.com>
2024-05-06 10:03:34 -0600

    [sys] cleanup venv prompt and activation
    Summary:
    Previously we were activating the default venv for each login shell, and
    reactivating for any interactive shell. This had unintended consequences
    on the PATH and on PS1. Here we avoid activating a venv that's already
    active and manage the venv component of PS1 ourselves. We also need
    to ensure that, at the conclusion of sh.local, we activate the default
    venv; so we de-activate before activating the default venv in sh.local
    to ensure the path is properly updated.
    
    Test Plan:
    
    Confirm that:
    - the venv prompt prefix appears as expected
    - /opt/homebrew/bin appears in the PATH in the expected order (e.g.,
    before /usr/local/bin) after opening login and non-login interactive
    shells
    - elements of PATH are ordered as expected in a vscode terminal that
    activates the default venv beforehand
    ```

--- system/files/usr/local/share/sitstart/util/py_env.sh
+++ system/files/usr/local/share/sitstart/util/py_env.sh
@@ -3,15 +3,14 @@
 # Activate a venv by path, and failing that, a conda env by name.
 # If no path or name is specified, defaults to `.venv`.
 function activate_py_env() {
-  if [ "$#" -lt 1 ]; then
-    env=./.venv
-  else
-    env="$1"
-  fi
-  # first, try activating as a virtual environment
+  local env="${1:-.venv}"
+  # first try activating as a virtual environment if it's not currently active
   if [ -d "$env" ]; then
-    # export the deactivate function for use in scripts/sub-shells
-    source "$env/bin/activate" && export -f deactivate
+    if [ -z "$VIRTUAL_ENV" ] ||
+      [ "$(realpath "$VIRTUAL_ENV")" != "$(realpath "$env")" ]; then
+      # activate and export the deactivate function for use in scripts/sub-shells
+      source "$env/bin/activate" && export -f deactivate
+    fi
   # otherwise use conda
   else
     source activate "$env" >/dev/null 2>&1
@@ -20,19 +19,19 @@ function activate_py_env() {
 
 # Deactivate any conda or virtual python environment.
 function deactivate_py_env() {
-  test -n "${VIRTUAL_ENV}" && deactivate
-  test -n "${CONDA_DEFAULT_ENV}" && conda deactivate
+  test -n "$VIRTUAL_ENV" && deactivate
+  test -n "$CONDA_DEFAULT_ENV" && conda deactivate
 }
 
 # Activate the default python environment. This is a venv called 'main' on
 # most machines, but it's a pre-installed conda 'pytorch' env on some configs.
-export DEFAULT_VENV="${HOME}/.virtualenvs/main"
+export DEFAULT_VENV="$HOME/.virtualenvs/main"
 function activate_default_py_env() {
   activate_py_env "$DEFAULT_VENV" || activate_py_env pytorch
 }
 
 # Reactivate the active python environment.
 function reactivate_py_env() {
-  test -n "${VIRTUAL_ENV}" && activate_py_env "${VIRTUAL_ENV}"
-  test -n "${CONDA_DEFAULT_ENV}" && activate_py_env "${CONDA_DEFAULT_ENV}"
+  test -n "$VIRTUAL_ENV" && activate_py_env "$VIRTUAL_ENV"
+  test -n "$CONDA_DEFAULT_ENV" && activate_py_env "$CONDA_DEFAULT_ENV"
 }
