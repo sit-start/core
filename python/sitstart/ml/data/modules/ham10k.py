723073a82799248759cd3582a49126b8290a9478
Kevin Dale <kevdale@gmail.com>
2024-06-11 13:52:21 -0600

    [ml] rebalancing sampler
    Addresses #122

--- python/sitstart/ml/data/modules/ham10k.py
+++ python/sitstart/ml/data/modules/ham10k.py
@@ -1,11 +1,13 @@
 from typing import cast
 
-from torch.utils.data import Subset
+from torch.utils.data import Sampler, Subset
 from torchvision.datasets.vision import VisionDataset
 
 from sitstart.logging import get_logger
 from sitstart.ml.data.datasets.ham10k import HAM10k as HAM10kDataset
 from sitstart.ml.data.modules.vision_data_module import VisionDataModule
+from sitstart.ml.util import rebalancing_sampler
+from sitstart.util.decorators import memoize
 
 logger = get_logger(__name__)
 
@@ -22,6 +24,17 @@ class HAM10k(VisionDataModule):
                 kwargs["shuffle"] = False
         self._rebalance = rebalance
         super().__init__(HAM10kDataset, *args, **kwargs)
+
+    @VisionDataModule.sampler.getter
+    @memoize
+    def sampler(self) -> Sampler | None:
+        if self._rebalance:
+            logger.info("Creating rebalancing sampler.")
+            train_dataset = cast(HAM10kDataset, self.train_dataset)
+            train_targets = train_dataset.targets
+            train_targets = [train_targets[i] for i in self.train_split.indices]
+            return rebalancing_sampler(train_targets, self.generator)
+        return None
 
     def _split_train_val(
         self, dataset: VisionDataset, **kwargs
