e172b0352ca824136d9bbd1964d6f521cb1341da
Kevin Dale <kevdale@gmail.com>
2024-06-24 03:49:33 -0600

    [exp] transform updates/additions
    Addresses #122

--- python/sitstart/ml/transforms.py
+++ python/sitstart/ml/transforms.py
@@ -1,5 +1,5 @@
 import torch
-from torchvision.transforms.functional import adjust_contrast
+from torchvision.transforms.functional import adjust_contrast, convert_image_dtype
 
 
 class AdjustContrast(torch.nn.Module):
@@ -12,3 +12,11 @@ class AdjustContrast(torch.nn.Module):
 
     def __repr__(self) -> str:
         return f"AdjustContrast(factor={self.factor})"
+
+
+class ImageToFloat32(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, img: torch.Tensor) -> torch.Tensor:
+        return convert_image_dtype(img, dtype=torch.float32)
