0acc3987eda3b4b7d25ca68d5f7439f6e83056da
Kevin Dale <kevdale@gmail.com>
2024-06-25 07:50:37 -0600

    [ray] incl param search space in job description
    Addresses #124

--- python/sitstart/util/hydra.py
+++ python/sitstart/util/hydra.py
@@ -1,11 +1,17 @@
 import operator
+from pathlib import Path
 from typing import Any
+from os.path import isabs, dirname, realpath, relpath
 
-from omegaconf import ListConfig, OmegaConf
+import hydra
+from omegaconf import Container, ListConfig, OmegaConf
 from torch import tensor
 
 from sitstart.logging import get_logger
 from sitstart.util.decorators import once
+
+VERSION_BASE = "1.2"
+CONFIG_PATH = str(Path(__file__).resolve().parents[1] / "ml" / "experiments" / "conf")
 
 logger = get_logger(__name__)
 
@@ -58,3 +64,23 @@ def register_omegaconf_resolvers():
     OmegaConf.register_new_resolver("round", round, replace=True)
     OmegaConf.register_new_resolver("abs", abs, replace=True)
     OmegaConf.register_new_resolver("int", lambda x: int(x), replace=True)
+
+
+def load_config(
+    name: str,
+    config_path: str,
+    overrides: list[str] | None = None,
+    version_base: str = VERSION_BASE,
+) -> Container:
+    """Loads an experiment config. For testing/debugging.
+
+    Args:
+        name: Name of the experiment config in `config_path`.
+        config_path: Absolute path to the config directory.
+        overrides: List of Hydra config overrides.
+        version_base: Hydra version base.
+    """
+    if isabs(config_path):
+        config_path = relpath(config_path, realpath(dirname(__file__)))
+    with hydra.initialize(version_base=version_base, config_path=config_path):
+        return hydra.compose(config_name=name, overrides=overrides)
